469027833a8804b7d4e82d478ee5f6c6fcc818e5
unknown <amierasyazlin97@gmail.com>
2019-12-03 01:41:13 +0700

    try json.parse

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let mahasiswa = { 
-    "name" => "Nur Syaqinah Binti Safaruddin",
-    "matric" => "193043405",
-    "email" => "qinah@gmail.com"
-}
-
-console.log(JSON.stringify(mahasiswa));
-
-// let xhr = new XMLHttpRequest();
-// xhr.onreadystatecjange = function () {
-//     if(xhr.readyState == 4 && xhr.status == 200) {
-//         let mahasiswa = JSON.parse(this.responseText);
-//         console.log(mahasiswa); 
-//     }
+// let mahasiswa = { 
+//     "name" => "Nur Syaqinah Binti Safaruddin",
+//     "matric" => "193043405",
+//     "email" => "qinah@gmail.com"
 // }
 
-// xhr.open('GET', 'coba.json', true);
-// xhr.send();
+// console.log(JSON.stringify(mahasiswa));
+
+let xhr = new XMLHttpRequest();
+xhr.onreadystatecjange = function () {
+    if(xhr.readyState == 4 && xhr.status == 200) {
+        let mahasiswa = JSON.parse(this.responseText);
+        console.log(mahasiswa); 
+    }
+}
+
+xhr.open('GET', 'coba.json', true);
+xhr.send();
