f59f551f0c90418acbd34728b2ba403728bb507e
unknown <amierasyazlin97@gmail.com>
2019-12-03 01:49:13 +0700

    json using jquery

--- script.js
+++ script.js
@@ -6,13 +6,17 @@
 
 // console.log(JSON.stringify(mahasiswa));
 
-let xhr = new XMLHttpRequest();
-xhr.onreadystatecjange = function () {
-    if(xhr.readyState == 4 && xhr.status == 200) {
-        let mahasiswa = JSON.parse(this.responseText);
-        console.log(mahasiswa); 
-    }
-}
+// let xhr = new XMLHttpRequest();
+// xhr.onreadystatecjange = function () {
+//     if(xhr.readyState == 4 && xhr.status == 200) {
+//         let mahasiswa = JSON.parse(this.responseText);
+//         console.log(mahasiswa); 
+//     }
+// }
 
-xhr.open('GET', 'coba.json', true);
-xhr.send();
+// xhr.open('GET', 'coba.json', true);
+// xhr.send();
+
+$.getJSON('coba.json', function (data) {
+        console.log(data);
+});
